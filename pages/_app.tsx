ef3d19b6f144304b647c9a6e2bdc0dd9c61099a9
Jay Nielsen <jayarnielsen@gmail.com>
2022-05-17 19:23:40 -0600

    switch to next because gatsby was stinky poo buns

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Sniglet, cursive",
+  },
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sniglet:wght@800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import { AspectRatio, Box, Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <Box
+      as="main"
+      css={{ maxWidth: "1200px", margin: "0 auto", padding: "2rem" }}
+    >
+      <Head>
+        <title>Rubbish Rabble</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Heading size="4xl" textAlign="center" marginBottom="2rem">
+        Rubbish Rabble
+      </Heading>
+      <AspectRatio>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/0iCuT7HkbJA"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </AspectRatio>
+    </Box>
+  );
+};
+
+export default Home;
