ed0ddbf956c2d1172a86bf88e7d42135705ea257
Jay Nielsen <jayarnielsen@gmail.com>
2022-07-13 14:10:29 -0600

    feat: bare bones studio drishti website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,29 @@
-import { AspectRatio, Box, Heading } from "@chakra-ui/react";
+import { Img, Box, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <Box
       as="main"
-      css={{ maxWidth: "1200px", margin: "0 auto", padding: "2rem" }}
+      css={{
+        display: "flex",
+        minHeight: "100vh",
+        maxWidth: "1200px",
+        margin: "0 auto",
+        padding: "2rem",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
     >
       <Head>
-        <title>Rubbish Rabble</title>
+        <title>Studio Drishti</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Heading size="4xl" textAlign="center" marginBottom="2rem">
-        Rubbish Rabble
-      </Heading>
-      <AspectRatio ratio={16 / 9}>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/0iCuT7HkbJA"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </AspectRatio>
+      <Img width="30%" src="/logo.svg" alt="Studio Drishti" />
+      <Text as="em">Focused Media</Text>
     </Box>
   );
 };
