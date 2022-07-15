3ea03c75582180cfbb7645657e52dd043aead583
jayarnielsen <jayarnielsen@gmail.com>
2022-07-15 21:18:26 +0000

    Deploying to gh-pages from @ studiodrishti/studio-drishti@72372ca83030d0cced792004a07b34655e0929fc 🚀

--- _next/static/02WDHX3DGtyQdl_Ne1IKl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/615-c5aa7bd7330f52fb.js","static/chunks/pages/index-4f9629ad7812e4e4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
